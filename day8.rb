9740b41597adbf3f4c6aa9649f6dbebbcff43d80
Hamzah Contreras <Hamzah_36@outlook.com>
2022-05-22 15:20:58 -0500

    Day8

---
+++ day8.rb
@@ -0,0 +1,28 @@
+# Ruby way to Factorial numbers
+
+def factorial_v1 number
+   result = number
+   while number > 1
+      result = result * (number - 1)
+      number = number - 1
+   end
+   puts "number #{number}"
+   return result
+end
+
+def factorial_v2 number
+   result = number
+   (1..(number - 1)).each do |item|
+      result = result * item
+   end
+   puts "number #{number}"
+   return result
+end
+
+def factorial_v3 number
+   (1..(number)).reduce(:*)
+end
+
+puts factorial_v1(5)
+puts factorial_v2(5)
+puts factorial_v3(5)
